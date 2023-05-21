f0fd9a7dee7546bc1e0bbfdeed704f6d13f33eff
Zadir7 <Zadir7@mail.ru>
2023-05-21 17:48:37 +0700

    HW2 - Small cleanup

--- ShootEmUp/Assets/Scripts/Components/TeamComponent.cs
+++ ShootEmUp/Assets/Scripts/Components/TeamComponent.cs
@@ -4,12 +4,6 @@ namespace ShootEmUp
 {
     public sealed class TeamComponent : MonoBehaviour
     {
-        public bool IsPlayer
-        {
-            get { return this.isPlayer; }
-        }
-        
-        [SerializeField]
-        private bool isPlayer;
+        [field: SerializeField] public bool IsPlayer { get; private set; }
     }
 }
