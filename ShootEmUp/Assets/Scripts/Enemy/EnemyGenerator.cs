02bf1f3583001b7232e606d188007aeccefe9e2b
Zadir7 <Zadir7@mail.ru>
2023-05-31 21:49:06 +0700

    HW2 Fix - Extracted enemy generation

---
+++ ShootEmUp/Assets/Scripts/Enemy/EnemyGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ShootEmUp
+{
+    public sealed class EnemyGenerator : MonoBehaviour
+    {
+        [SerializeField] private EnemySpawner enemySpawner;
+        
+        private IEnumerator Start()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+                
+                this.enemySpawner.SpawnEnemy();
+            }
+        }
+    }
+}
--- ShootEmUp/Assets/Scripts/Enemy/EnemySpawner.cs
+++ ShootEmUp/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -11,23 +11,11 @@ namespace ShootEmUp
         [SerializeField] private EnemyPool enemyPool;
 
         public event Action<GameObject> EnemySpawned;
-        
-        private IEnumerator Start()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(1);
-                
-                var enemy = this.SpawnEnemy();
-                if (enemy == null) continue;
-                
-                EnemySpawned?.Invoke(enemy);
-            }
-        }
 
-        private GameObject SpawnEnemy()
+        public void SpawnEnemy()
         {
             var enemy = this.enemyPool.SpawnEnemy();
+            if (enemy is null) return;
             
             var spawnPosition = this.enemyPositions.GetRandomSpawnPosition();
             enemy.transform.position = spawnPosition.position;
@@ -35,8 +23,8 @@ namespace ShootEmUp
             
             enemy.GetComponent<EnemyMoveAgent>().SetDestination(attackPosition.position);
             enemy.GetComponent<EnemyAttackAgent>().SetTarget(this.character.gameObject);
-
-            return enemy;
+            
+            EnemySpawned?.Invoke(enemy);
         }
 
         public void DespawnEnemy(GameObject enemy)
