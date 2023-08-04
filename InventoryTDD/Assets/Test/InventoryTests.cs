d1f9e324ac7348a87ecda35ead150ccfa948a780
Zadir7 <Zadir7@mail.ru>
2023-08-04 19:17:56 +0700

    HW10 - Added project

---
+++ InventoryTDD/Assets/Test/InventoryTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.Test
+{
+    [TestFixture]
+    public sealed class InventoryTests
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
